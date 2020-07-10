6e14e66c74bb81eea8039dd04c5e2c5c95048e40
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2020-07-11 00:55:31 +0200

    Add task

--- AddNoteTranscribing.Designer.cs
+++ AddNoteTranscribing.Designer.cs
@@ -30,98 +30,66 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox_Project = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDown_LectureNumber = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.button_Add = new System.Windows.Forms.Button();
             this.button_Cancel = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_LectureNumber)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(11, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 24);
+            this.label1.Size = new System.Drawing.Size(52, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Projekt";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // comboBox_Project
             // 
             this.comboBox_Project.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_Project.FormattingEnabled = true;
-            this.comboBox_Project.Location = new System.Drawing.Point(16, 32);
+            this.comboBox_Project.Location = new System.Drawing.Point(15, 26);
+            this.comboBox_Project.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox_Project.MaxDropDownItems = 100;
             this.comboBox_Project.Name = "comboBox_Project";
-            this.comboBox_Project.Size = new System.Drawing.Size(332, 28);
+            this.comboBox_Project.Size = new System.Drawing.Size(348, 24);
             this.comboBox_Project.TabIndex = 0;
-            this.comboBox_Project.SelectedIndexChanged += new System.EventHandler(this.comboBox_Project_SelectedIndexChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 75);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(139, 24);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Föreläsning Nr.";
-            // 
-            // numericUpDown_LectureNumber
-            // 
-            this.numericUpDown_LectureNumber.Location = new System.Drawing.Point(16, 102);
-            this.numericUpDown_LectureNumber.Maximum = new decimal(new int[] {
-            1000,
-            0,
-            0,
-            0});
-            this.numericUpDown_LectureNumber.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.numericUpDown_LectureNumber.Name = "numericUpDown_LectureNumber";
-            this.numericUpDown_LectureNumber.Size = new System.Drawing.Size(332, 26);
-            this.numericUpDown_LectureNumber.TabIndex = 1;
-            this.numericUpDown_LectureNumber.Value = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.numericUpDown_LectureNumber.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 148);
+            this.label3.Location = new System.Drawing.Point(11, 54);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 20);
+            this.label3.Size = new System.Drawing.Size(128, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "Föreläsningsdatum";
             // 
-            // dateTimePicker1
+            // dateTimePicker
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(16, 171);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
-            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker.Location = new System.Drawing.Point(15, 73);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(348, 22);
+            this.dateTimePicker.TabIndex = 2;
             // 
             // button_Add
             // 
-            this.button_Add.Location = new System.Drawing.Point(53, 238);
+            this.button_Add.Location = new System.Drawing.Point(15, 130);
+            this.button_Add.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_Add.Name = "button_Add";
-            this.button_Add.Size = new System.Drawing.Size(112, 70);
+            this.button_Add.Size = new System.Drawing.Size(172, 55);
             this.button_Add.TabIndex = 5;
             this.button_Add.Text = "Lägg Till";
             this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
             // 
             // button_Cancel
             // 
-            this.button_Cancel.Location = new System.Drawing.Point(171, 238);
+            this.button_Cancel.Location = new System.Drawing.Point(193, 130);
+            this.button_Cancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button_Cancel.Name = "button_Cancel";
-            this.button_Cancel.Size = new System.Drawing.Size(112, 70);
+            this.button_Cancel.Size = new System.Drawing.Size(172, 55);
             this.button_Cancel.TabIndex = 5;
             this.button_Cancel.Text = "Avbryt";
             this.button_Cancel.UseVisualStyleBackColor = true;
@@ -129,25 +97,23 @@
             // 
             // AddNoteTranscribing
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 434);
+            this.ClientSize = new System.Drawing.Size(379, 254);
             this.ControlBox = false;
             this.Controls.Add(this.button_Cancel);
             this.Controls.Add(this.button_Add);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dateTimePicker);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.numericUpDown_LectureNumber);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox_Project);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AddNoteTranscribing";
             this.Text = "Lägg till Renskrivning";
             this.Load += new System.EventHandler(this.AddNoteTranscribing_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_LectureNumber)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,10 +123,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox_Project;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.NumericUpDown numericUpDown_LectureNumber;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
         private System.Windows.Forms.Button button_Add;
         private System.Windows.Forms.Button button_Cancel;
     }
--- AddNoteTranscribing.cs
+++ AddNoteTranscribing.cs
@@ -8,15 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Todoist.Net;
+using Todoist.Net.Models;
 
 namespace Todoist_Automation
 {
-    
+
     public partial class AddNoteTranscribing : Form
     {
 
         string tdEmail = "robin.claesson@gmail.com", tdPassword = "psom@un2krok0PREA";
-        
+        ITodoistTokenlessClient tokenlessClient;
+        ITodoistClient client;
+
+        long labelID = 0;
 
         public AddNoteTranscribing()
         {
@@ -32,31 +36,32 @@ namespace Todoist_Automation
 
         private async void LoadTodoistData()
         {
-            ITodoistTokenlessClient tokenlessClient = new TodoistTokenlessClient();
-            ITodoistClient client = await tokenlessClient.LoginAsync(tdEmail, tdPassword);
+            //Loggar in på Todoist
+            //TODO: Logga in med token istället för email
+            tokenlessClient = new TodoistTokenlessClient();
+            client = await tokenlessClient.LoginAsync(tdEmail, tdPassword);
 
+            //Hämtar alla projectnamn och lägger till dom som alternativ
             var projects = await client.Projects.GetAsync();
-            foreach(var proj in projects)
+            foreach (var proj in projects)
             {
-                
                 comboBox_Project.Items.Add(proj.Name);
+            }
+
+            //Hämtar LabelID för renskrivning
+            var labels = await client.Labels.GetAsync();
+            foreach (var label in labels)
+            {
+                if (label.Name == "Renskrivning")
+                    labelID = label.ItemOrder;
             }
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
 
-        private void comboBox_Project_SelectedIndexChanged(object sender, EventArgs e)
-        {
 
-        }
 
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
 
-        }
 
         private void button_Cancel_Click(object sender, EventArgs e)
         {
@@ -64,10 +69,68 @@ namespace Todoist_Automation
             this.Close();
         }
 
-        private void SetControlsEnable(bool enable)
+        private void button_Add_Click(object sender, EventArgs e)
         {
-            foreach (Control control in Controls)
-                control.Enabled = enable;
+
+            AddTask();
+            this.DialogResult = DialogResult.OK;
+        }
+
+
+        private async void AddTask()
+        {
+            //Hämtar alla projekt
+            var projects = await client.Projects.GetAsync();
+            foreach (var proj in projects)
+            {
+                //Hittar projektet som stämmer överens med valet
+                if (proj.Name == comboBox_Project.Text)
+                {
+
+                    ////Lägger till uppgiften
+                    //var mainTaskID = await client.Items.AddAsync(new Item("Renskrivning föreläsning " + comboBox_Project.Text + " " + dateTimePicker.Value.Day + "/" + dateTimePicker.Value.Month, proj.Id));
+
+                    ////Hämtar uppgiften för att komma åt dess position i listan
+                    //var mainTaskChildOrder = 0;
+                    //var tasks = await client.Items.GetAsync();
+                    //foreach (Item task in tasks)
+                    //    if (task.Id == mainTaskID)
+                    //        mainTaskChildOrder = (int)task.ChildOrder;
+
+                    ////Lägger till underuppgifter 
+                    //List<ComplexId> subtaskIDs = new List<ComplexId>();
+                    //subtaskIDs.Add(await client.Items.AddAsync(new Item("Scanna orginal", proj.Id)));
+                    ////subtaskIDs.Add(await client.Items.AddAsync(new Item("Renskriva", proj.Id)));
+                    ////subtaskIDs.Add(await client.Items.AddAsync(new Item("Sammanfatta", proj.Id)));
+
+                    ////Hämtar hem alla uppgifterna igen och loopar igenom efter underuppgifterna och uppdaterar dom som en underuppgift
+                    //tasks = await client.Items.GetAsync();
+                    //foreach (Item task in tasks)
+                    //{
+                    //    if(subtaskIDs.Contains(task.Id))
+                    //    {
+                    //        task.ParentId = mainTaskChildOrder;
+                    //        await client.Items.UpdateAsync()
+                    //    }
+                    //}
+
+
+                    //tasks = await client.Items.GetAsync();
+
+
+                    var transaction = client.CreateTransaction();
+
+
+                    var taskId = await transaction.Items.AddAsync(new Item("Renskrivning föreläsning " + comboBox_Project.Text + " " + 
+                        dateTimePicker.Value.Day + "/" + dateTimePicker.Value.Month, proj.Id));
+
+                    transaction.Items.
+
+                    await transaction.CommitAsync();
+
+                }
+            }
+
         }
     }
 }
